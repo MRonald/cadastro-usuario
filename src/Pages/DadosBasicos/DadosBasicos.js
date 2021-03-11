9906d0f2288e989b0d56d4299c5094730d0badb6
MRonald <ronaldmichael918@gmail.com>
2021-03-11 02:51:02 -0300

    Adicionando uso da const no DadosBasicos.js | Removendo barra de rolagem cross-browser

--- src/Pages/DadosBasicos/DadosBasicos.js
+++ src/Pages/DadosBasicos/DadosBasicos.js
@@ -2,6 +2,6 @@ window.onload = function () {
     const INPUT_FILE = document.getElementById('foto');
     const TAG_IMG = document.getElementById('img-preview');
     if (INPUT_FILE != null) {
-        INPUT_FILE.addEventListener('change', () => document.getElementById('img-preview').src = window.URL.createObjectURL(INPUT_FILE.files[0]));
+        INPUT_FILE.addEventListener('change', () => TAG_IMG.src = window.URL.createObjectURL(INPUT_FILE.files[0]));
     }
 }
